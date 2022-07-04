696f51ffca1498d1fa5b09e28d2bcba2c03d41dd
Priyanshu <priyanshu0102here@gmail.com>
2022-07-04 16:37:28 +0530

    Updated other features

--- partials/_logouthandling.php
+++ partials/_logouthandling.php
@@ -4,5 +4,5 @@ session_start();
 session_reset();
 session_destroy();
 // echo "Session is destroyed";  
-header("Location: /forum/index.php");  
+header("Location: /project/index.php");  
 ?>
--- partials/navbar.php
+++ partials/navbar.php
@@ -17,9 +17,16 @@ echo '<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
         <li class="nav-item">
           <a class="nav-link" href="#">About</a>
          </li>
-  
-      </ul>
-    </div>
+         
+         </ul>';
+
+         if(isset($_SESSION['loggedin']) && ($_SESSION['loggedin'])==true ){
+         echo '<div class="logout d-flex justify-content-right">
+         <a href="partials/_logouthandling.php" class="btn btn-outline-warning">LOG OUT</a>
+
+       </div>';
+         }
+      echo '  </div>
   </div>
 </nav>';
 
@@ -34,14 +41,14 @@ if(isset($_GET['signupsuccess']) && $_GET['signupsuccess']=="true")
 if(isset($_GET['signupsuccess']) && $_GET['signupsuccess']=="false")
 {
   echo '<div class="alert alert-danger alert-dismissible" role="alert">
-              <strong>ERROR!</strong> Email is already taken <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              <strong>ERROR!</strong> Username is already taken <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
               </div>';
 }
 
 if(isset($_GET['loginsuccess']) && $_GET ['loginsuccess']=="false")
 {
   echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-  <strong>Failure!</strong> You were not able to login because password didn\'t match !!
+  <strong>Failure!</strong> You were not able to login because password didn\'t match or username already taken!!
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>';
 }
@@ -50,6 +57,22 @@ if(isset($_GET['loginsuccess']) && $_GET ['loginsuccess']=="true")
 {
   echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
   <strong>Success!</strong> You have logged in!!
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+}
+if(isset($_GET['insert']) && $_GET ['insert']=="true")
+
+{
+  echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <strong>Success!</strong> Data inserted succesfully, Do not add data twice on the same adv!!
+  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>';
+}
+if(isset($_GET['insert']) && $_GET ['insert']=="false")
+
+{
+  echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <strong>Something Went Wrong!</strong> Error occur data is not inserted!!
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>';
 }
